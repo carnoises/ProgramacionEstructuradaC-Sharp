2e93e41495102b269e60ceefb6fc103e9ba0bedd
carnoises <50802944+carnoises@users.noreply.github.com>
2021-10-14 13:10:41 -0400

    Ejemplo ArrayList

---
+++ EjemploArrayList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objeto de la coleccion ArrayList
+            ArrayList alumnos = new ArrayList();
+
+            //Llenando de alumnos la coleccion
+            alumnos.Add("Lucy Pacheco Hernandez");
+            alumnos.Add("Carla Gallegos Silva");
+            alumnos.Add("Heidi Rengifo Reategui");
+
+            //Mostrando los registro 
+            foreach (String a in alumnos)
+            {
+                MessageBox.Show(a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
