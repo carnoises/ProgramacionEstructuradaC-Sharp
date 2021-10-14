4df0bc3462f981e05104c71ea3a794455247bf5b
carnoises <50802944+carnoises@users.noreply.github.com>
2021-10-14 12:56:20 -0400

    Ejemplo con Foreach

---
+++ EjemploForeach/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Objeto de la coleccion List
+            List<string> productos = new List<string>();
+
+            //Llenado de productos la coleccion
+            productos.Add("Impresora");
+            productos.Add("Mouses");
+            productos.Add("Teclado");
+            productos.Add("Parlante");
+
+            //Mostrando los regsitro
+            foreach (var p in productos)
+            {
+                MessageBox.Show(p);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
